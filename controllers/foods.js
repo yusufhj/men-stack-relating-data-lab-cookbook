92501d21ee911134e06ce8333bd2cce89d7b9bba
Yusuf AlHamad <ylive71@gmail.com>
2024-10-24 14:14:04 +0300

    Build the new page

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,8 +3,33 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/', (req, res) => {
-    res.render('foods/index.ejs')
+router.get('/', async (req, res) => {
+    try {
+        const user = await User.findById(req.session.user._id);
+        console.log(user.pantry);
+        // res.locals.user.pantry = userPantry;
+        console.log(user.pantry);
+        res.render('foods/index.ejs', { user });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+router.get('/new', (req, res) => {
+    res.render('foods/new.ejs')
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const user = await User.findById(req.session.user._id)
+        user.pantry.push(req.body)
+        user.save()
+        res.redirect(`/users/${user._id}/foods`);
+    } catch (error) {
+        res.send(error);
+        res.redirect('/')
+    }
 });
 
 module.exports = router;
